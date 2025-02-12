19848e7a90378dbef72a405b7d89822b83d89d30
nancyzhan <1020449768@qq.com>
2025-02-12 11:44:52 +0800

    fix: 删除useLocale的ref

--- packages/traction-widget/components/hooks/useLocale.ts
+++ packages/traction-widget/components/hooks/useLocale.ts
@@ -1,4 +1,3 @@
-import { ref } from 'vue';
 import zhCN from '../locales/lang/zhCN.ts';
 import enUS from '../locales/lang/enUS.ts';
 
@@ -6,18 +5,10 @@ import enUS from '../locales/lang/enUS.ts';
 type Locale = Record<string, any>;
 
 export function useLocale (): Locale {
-    const locale = ref<Locale>({});
-    let storedLocale = 'zh-CN';  // 默认使用中文
-    
-    // 只在浏览器环境下读取 localStorage
+    let storedLocale = 'zh-CN';
     if (typeof window !== 'undefined') {
         storedLocale = localStorage.getItem('fes_locale') || 'zh-CN';
     }
 
-    if (storedLocale === 'en-US') {
-        locale.value = enUS;
-    } else {
-        locale.value = zhCN;
-    }
-    return locale.value;
+    return storedLocale === 'en-US' ? enUS : zhCN;
 }
