1029e89be9d17b2a488a355bb1dd75413a9b1011
mayinrain <624899618@qq.com>
2025-02-12 10:18:35 +0800

    upd: 按需引入echarts组件

--- packages/traction-widget/components/Charts/useEcharts.ts
+++ packages/traction-widget/components/Charts/useEcharts.ts
@@ -1,15 +1,12 @@
 // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
 import * as echarts from 'echarts/core';
 // 引入柱状图图表，图表后缀都为 Chart
-import * as charts from 'echarts/charts';
+import { BarChart } from 'echarts/charts';
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
-import * as renderers from 'echarts/renderers';
+import { CanvasRenderer } from 'echarts/renderers';
 // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
-import * as components from 'echarts/components';
+import { TooltipComponent, GridComponent, DataZoomComponent, LegendComponent } from 'echarts/components';
 
-const { BarChart } = charts;
-const { CanvasRenderer } = renderers;
-const { TooltipComponent, GridComponent, DataZoomComponent, LegendComponent } = components;
 // 注册必须的组件
 echarts.use([
     TooltipComponent,
